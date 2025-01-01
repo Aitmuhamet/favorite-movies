8472b7c4dd616cc5fcb2962b693cab0c8ac62a0e
Aitmukhamet Khojakeldiyev <xa210296@gmail.com>
2025-01-01 15:06:49 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <main>
+    <header class="header">
+      <img
+        src="/logo.svg"
+        alt="Logo"
+        class="header-logo"
+      />
+      <h1>My Favorite Movies</h1>
+    </header>
+    <div class="tabs">
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 1 }]"
+        @click="setTab(1)"
+      >Favorite</button>
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 2 }]"
+        @click="setTab(2)"
+      >Search</button>
+    </div>
+    <div
+      class="movies"
+      v-if="movieStore.activeTab === 1"
+    >
+      <div>
+        <h3>Watched Movies (count: {{ movieStore.watchedMovies.length }})</h3>
+        <Movie
+          v-for="movie in movieStore.watchedMovies"
+          :key="movie.id"
+          :movie="movie"
+        />
+      </div>
+      <h3>All Movies (count: {{ movieStore.movies.length }})</h3>
+      <Movie
+        v-for="movie in movieStore.movies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+    <div
+      class="search"
+      v-else
+    >
+      <Search />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import Search from './components/Search.vue';
+import { useMovieStore } from './stores/MovieStore';
+import Movie from './components/Movie.vue';
+const movieStore = useMovieStore();
+
+const setTab = (tab) => {
+  movieStore.setActiveTab(tab);
+}
+</script>
+
+
+<style>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+
+.search {
+  width: 100%;
+  margin-top: 35px;
+}
+
+.header-logo {
+  max-width: 50px;
+  margin-right: 10px;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+}
+
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+
+.btn:hover {
+  opacity: 0.7;
+}
+
+.btn_green {
+  background: #37df5c;
+}
+
+s .tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="">
+    <input
+      type="text"
+      class="search-input"
+      placeholder="Search for a movie..."
+      v-model="searchMovie"
+    />
+    <button type="submit">Search</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const searchMovie = ref('');
+</script>
+
+<style scoped>
+.search-input {
+  border: 1px solid #e7e7e7;
+  width: 100%;
+  height: 40px;
+  padding: 0 10px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+}
+.search-input:hover {
+  outline: 1px solid blue;
+}
+</style>
